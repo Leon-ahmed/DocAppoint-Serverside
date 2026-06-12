725757761405664cf1892e7a47008f403f12850d
Leon-ahmed <mdleon1200@gmail.com>
2026-06-12 19:41:18 +0600

    Mondodb connetion setup with backend done

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
-const express = require ('express');
-const app = express();
+ 
+const dns = require('node:dns');
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+ 
 
-const PORT = process.env.PORT || 5000;
+
+const express = require ('express');
+const dotenv = require("dotenv");
+dotenv.config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+app.use(express.json());
+
+
+const uri = process.env.MONGODB_URI;
+const PORT = process.env.PORT ;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+async function run() {
+  try {
+    
+    await client.connect();
+    
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
 
 app.get('/',(req,res)=>{
-    res.send('server is fine');
+    res.send('server is fine. Welcome');
 })
 
 
